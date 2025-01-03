67d8013c2101de1589e75dd46e249aafc1dfe5a8
ANKIT3058 <147648692+ANKIT3058@users.noreply.github.com>
2025-01-03 13:43:57 +0000

    Learning variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144383
+let accountEmail = "ankit@gmail.com"
+var accountPassword = "dieow230"
+accountCity = "Prayagraj"
+let accountState;
+
+/*
+Prefer not to use var
+due to issues of block scope and functional scope
+*/
+
+// console.log(accountCity);
+console.table([accountId, accountEmail, , accountPassword, accountCity, accountState])
+
