b3fead46c9f74bb1df36a50ff2da0f87ec8b3297
ANKIT3058 <147648692+ANKIT3058@users.noreply.github.com>
2025-01-03 15:01:44 +0000

    conversion of data types

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,26 @@
+/*
+let age = 33 -> number but Number is a class
+age = String(age)
+
+let stringAge = String(age)
+console.log(typeof age)
+console.log(typeof stringAge)
+*/
+
+// Trying to convert string to number 
+/*
+let num = "33"
+Number(num)
+"33" => 33 number
+"33abc" => NaN number
+"ankit" => NaN, "" => 0
+true => 1; false => 0
+*/
+//let num=true
+//console.log(Number(num))
+
+// String to Boolean
+/*
+"" => false
+"hitesh" => true
+*/
