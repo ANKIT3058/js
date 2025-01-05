26f460d47d9fa5f56224d88e6ad3184281a94bd0
ANKIT3058 <147648692+ANKIT3058@users.noreply.github.com>
2025-01-05 13:01:44 +0000

    Data types summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,27 @@
+// Javascript is a dynamically typed language
+/*
+let a;
+a=0;
+console.log(typeof a);  -> number
+a="Hello world";
+console.log(typeof a);  -> string
+a=true;
+console.log(typeof a);  -> boolean
+*/
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof myFunction); -> function
+// console.log(typeof myObj);      -> object
+// console.log(typeof heros);      -> object
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
