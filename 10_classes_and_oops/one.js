a36d9607909f3cbed58228a968522d307fc551b6
ANKIT3058 <147648692+ANKIT3058@users.noreply.github.com>
2025-01-14 16:59:48 +0000

    prototype and objects in js

---
+++ 10_classes_and_oops/one.js
@@ -0,0 +1,43 @@
+const user = {
+    username: "hitesh",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
+
+// What happens when we use new keyword
+// Step 1: new object is created
+// Step 2: constructor function is called because of new keyword, it packs all the arguments
+// Step 3: this keyword and all the arguments are injected in object
+// Step 4: you get the object
